64b62420d6163e71a9ca29925d7c938a1b714189
Ralph Wessel <ralph@activethread.net>
2024-08-19 19:11:44 +0100

    Version bump

--- Active/Version.h
+++ Active/Version.h
@@ -10,6 +10,6 @@ static const unsigned int versionMajor = 0;
 
 static const unsigned int versionMinor = 9;
 
-static const unsigned int versionPatch = 1;
+static const unsigned int versionPatch = 2;
 
 #endif	//ACTIVE_VERSION
